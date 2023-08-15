a8479ceebcd50857c8661774dfd0abdab3db1742
islamsamy214 <islamsamy214@gmail.com>
2023-08-15 13:41:36 +0300

    remove the score from the critera

--- app/Models/Criteria.php
+++ app/Models/Criteria.php
@@ -24,19 +24,4 @@ class Criteria extends Model
     {
         return $this->belongsToMany(Team::class, 'teams_criterias')->withPivot('id', 'score');
     } // end of teams
-
-    // trigger a function on update
-    public static function boot()
-    {
-        parent::boot();
-        static::updated(function ($criteria) {
-            $teams = $criteria->teams()->get();
-            $score = 0;
-            foreach ($teams as $team) {
-                $score += $team->pivot->score;
-            }
-            // update the level score
-            $criteria->evaluation->level->teams()->updateExistingPivot($team->id, ['score' => $score]);
-        });
-    } // end of boot
 }
