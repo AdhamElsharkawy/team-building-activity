abb7795fb9a6c7a2b60ecdbe78030b678346f809
Ubuntu <ubuntu@ip-172-31-35-81.eu-west-1.compute.internal>
2023-08-28 09:24:57 +0000

    dashboard adj

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -21,7 +21,7 @@ class DashboardController extends Controller
         }])
         ->withCount('users')
         ->orderBy('score', 'DESC')->get();
-
+        
         $teams = $this->hideUnnecessaryData($teams);
         $seo = Seo::first();
         return $this->apiSuccessResponse(
@@ -33,18 +33,26 @@ class DashboardController extends Controller
 
     private function hideUnnecessaryData($teams)
     {
-        $teams->makeHidden(["created_at", "updated_at", "image"]);
-        $teams->each(function ($team) {
-            $team->levels->makeHidden(["pivot", "created_at", "updated_at"]);
-            // append the score form the pivot table to the level
-            $team->levels->each(function ($level) {
-                $level->score = $level->pivot->score * 100;
+        try{
+            $teams->makeHidden(["created_at", "updated_at", "image"]);
+            $teams->each(function ($team) {
+                $team->levels->makeHidden(["pivot", "created_at", "updated_at"]);
+                // append the score form the pivot table to the level
+                $team->levels->each(function ($level) {
+                    $level->score = $level->pivot->score * 100;
+                });
+                $team->levels->first()->evaluations->makeHidden(["level_id"]);
+                $team->levels->first()->evaluations->each(function ($evaluation) {
+                    $evaluation->criteria->makeHidden(["evaluation_id"]);
+                });
             });
-            $team->levels->first()->evaluations->makeHidden(["level_id"]);
-            $team->levels->first()->evaluations->each(function ($evaluation) {
-                $evaluation->criteria->makeHidden(["evaluation_id"]);
-            });
-        });
-        return $teams;
+            return $teams;
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Something went wrong, please try again later',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     } // end of hideUnnecessaryData
 }
